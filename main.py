e82184f296787c404c8241b8f5d3386a66b566d1
Rich <richkitibwa@gmail.com>
2022-03-16 01:19:21 +0300

    add application context

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
-from config import create_app
+from config import create_app, db
 import commands
 
 main = Blueprint('main', __name__)
@@ -22,4 +22,5 @@ app = create_app()
 commands.init_app(app)
 
 if __name__ == '__main__':
+    db.create_all(app=create_app())
     app.run(debug=True)
